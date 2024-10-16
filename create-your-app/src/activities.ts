351fd894bd055fcc907ff7ff70abcab3b4bb8012
angelazhou32 <angela.zhou@temporal.io>
2024-10-16 10:26:46 -0400

    formatting

--- create-your-app/src/activities.ts
+++ create-your-app/src/activities.ts
@@ -1,5 +1,7 @@
 /*
-Activities are the building blocks of a Temporal Workflow. They encapsulate the logic for tasks that interact with external services such as querying a database or calling a third-party API. One of the key benefits of using Activities is their built-in fault tolerance. If an Activity fails, Temporal can automatically retry it until it succeeds or reaches a specified retry limit. This ensures that transient issues, like network glitches or temporary service outages, don't result in data loss or incomplete processes.
+Activities are the building blocks of a Temporal Workflow. They encapsulate the logic for tasks that interact with external services such as querying a database or calling a third-party API. 
+
+One of the key benefits of using Activities is their built-in fault tolerance. If an Activity fails, Temporal can automatically retry it until it succeeds or reaches a specified retry limit. This ensures that transient issues, like network glitches or temporary service outages, don't result in data loss or incomplete processes.
 
 To keep things focused on the Workflow logic, these Activities will only contain basic log statements.
 */
